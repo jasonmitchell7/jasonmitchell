06249408705c658c331ebdd4dd4c1ad542e811cb
J Jason Mitchell <jasonmitchell7@gmail.com>
2017-06-09 21:25:38 -0500

    updated about me

--- src/js/components/AboutMe.js
+++ src/js/components/AboutMe.js
@@ -6,20 +6,33 @@ export default class AboutMe extends React.Component {
             <div>
                 <h1>About Me</h1>
                 <p>
-                    Converging creativity with coding is my passion. I have a creative background in film and studied
-                    computer science.
+                    Converging creativity with coding is my passion. I have a creative background in film and an
+                    education in computer science. The vast areas of concern while creating a piece of software is
+                    something I love. From trying to figure out the best approach for time and space efficiency, to
+                    meeting project deadlines and lastly, creating an elegant and intuitive user experience. The
+                    details are important. Hamburger menus that animate into an "X" make me happy.
                 </p>
                 <p>
-                    I've had a love for programming since I was a young child. This is largely due to my father, who used
-                    my parent's wedding money to buy a Macintosh IIfx. A few years later, I fell in love with an online
-                    game, but could not afford to pay the monthly subscription fee. I ended up contributing to a
-                    server-side emulator for the game. This included writing tutorials for C# development specific to use
-                    with the emulator.
+                    While pursuing my degree at the University of Minnesota, I created a social media app. This used
+                    Swift for the front end (iOS) and Ruby On Rails for the backend API. This was an incredible
+                    learning experience. Leveraging the power of both the AVFoundation and CoreImage frameworks allowed
+                    me to implement face tracking and a filter system. Content was uploaded to an Amazon AWS S3 server.
+                    I also used other Amazon AWS services during this experience, which included RDS and EC3. These
+                    were deployed using a docker container. Ultimately, I learned that I love the coding a creating
+                    parts of building a larger app, but not the business aspects. Trying secure capital is not
+                    something I wish to do again, and this project taught me that I am a person that is looking for
+                    more security in my career.
                 </p>
                 <p>
-                    I love when people doubt me as it simply provides an opportunity to prove them wrong. I was told when I
-                    was 14 that I had a social phobia and that this was something that could not be overcome. Shortly
-                    thereafter, I was registered for an acting class. Now I love talking to people and in front of people.
+                    Also during my schooling I was an intern at Infinite Campus. This was a wonderful experience with
+                    many great people. It gave me great insight into how a larger software operates. I was able to
+                    join an agile team for multiple projects, which ranged from updating the backend REST API (Java)
+                    to making significant UI changes while adding new features. I also got to spend a great amount of
+                    time working on tests and automation.
+                </p>
+                <p>
+                    I believe that these experiences make me a very strong candidate for a junior software engineer
+                    or full stack developer position.
                 </p>
             </div>
         );
